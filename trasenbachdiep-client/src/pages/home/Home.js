bc79818ab1a299c7cc431b4b756d1dfa98f597b1
TrungTien1123 <11218223@st.neu.edu.vn>
2024-05-13 00:24:59 +0700

    move to scss

--- trasenbachdiep-client/src/pages/home/Home.js
+++ trasenbachdiep-client/src/pages/home/Home.js
@@ -1,3 +1,4 @@
+import clsx from 'clsx'
 import banner from'../../images/trangchu/banner.png'
 import product01 from "../../images/trangchu/product01.png"
 import product02 from "../../images/trangchu/product02.png"
@@ -14,92 +15,92 @@ import person03 from "../../images/trangchu/person03.png"
 import person04 from "../../images/trangchu/person04.png"
 import person05 from "../../images/trangchu/person05.png"
 
-import './trangchu.css'
+import styles from './Home.module.scss'
 import ControlledCarousel from './carousel/Carousel'
 import { Link } from 'react-router-dom'
 
 function Home(){
     return(
         <div>
-        <ControlledCarousel className="carousel-home"/>
-        <div className="content-home pt-5 ">
-            <div className="signature pt-5 col-8 m-auto ">
-                <h2 className="text-center title">SẢN PHẨM NỔI BẬT</h2>
+        <ControlledCarousel className={styles.carousel}/>
+        <div className="content-home pt-5 ">ee
+            <div className= {clsx(styles.signature, 'pt-5' , 'col-8', 'm-auto')}>
+                <h2 className= {clsx(styles.title, 'text-center')}>SẢN PHẨM NỔI BẬT</h2>
                 <div className="list-product justify-content-between d-flex pt-4 position-relative">
-                    <div className="product">
+                    <div className={styles.product}>
                         <img src={product01} alt=""/>
                         <h5 className="text-center pt-3">TRÀ ƯỚP BÔNG SEN</h5>
                     </div>
-                    <div className="product">
+                    <div className={styles.product}>
                         <img src={product02} alt=""/>
                         <h5 className="text-center pt-3">TRÀ ƯỚP NHỊ SEN</h5>
                     </div>
-                    <div className="product">
+                    <div className={styles.product}>
                         <img src={product03} alt=""/>
                         <h5 className="text-center pt-3">TRÀ Ô LONG</h5>
                     </div>
-                    <div className="product">
+                    <div className={styles.product}>
                         <img src={product04} alt=""/>
                         <h5 className="text-center pt-3">TRÀ SHAN TUYẾT</h5>
                     </div>
-                <img className="btn-left position-absolute" src={nextleft} alt=""/>
-                <img className="btn-right position-absolute" src={nextright} alt=""/>
-                <img className="list-product-back position-absolute" src={listback} alt=""/>
+                <img className= {clsx(styles.btnLeft, 'position-absolute')} src={nextleft} alt=""/>
+                <img className={clsx(styles.btnRight, 'position-absolute')} src={nextright} alt=""/>
+                <img className={clsx(styles.listProductBack, 'position-absolute')} src={listback} alt=""/>
                 </div>
                
             </div>
-            <div className="introduction position-relative">
+            <div className= {clsx(styles.introduction, 'position-relative')}>
                 <div className="intro-content">
-                    <h2 className="intro-title text-center title">GIỚI THIỆU</h2>
+                    <h2 className= {clsx(styles.introTitle, styles.title, 'text-center')}>GIỚI THIỆU</h2>
                     <div>
-                        <div className="intro-caption d-none d-md-block ">
+                        <div className= {clsx(styles.introCaption,'d-none','d-md-block')}>
                             <h1 className="">Trà sen Bách Diệp</h1>
-                            <p className="intro-infor text-justify">
+                            <p className={clsx(styles.introInfo, 'text-justify')}>
                             Trà sen bách diệp, với hương thơm dịu dàng và vị tinh tế, 
                             là biểu tượng của sự thanh nhã và sự tinh tế trong ẩm thực trà. 
                             Với hương thơm dịu nhẹ, vị thanh mát, nó làm say đắm lòng người, mang lại cảm giác bình yên và thư thái.
                             </p>
-                            <div className="btn">
+                            <div className={styles.btn}>
                                 <Link to={'/about'}> ĐỌC THÊM</Link>                       
                             </div>
                         </div>
                     </div>
                 </div>
-                <div className="contact position-absolute d-flex">
-                    <div className="phone"><img src={phone} alt=""/></div>
-                    <div className="phone-number"> <h4>9999.999.999</h4></div>
+                <div className={clsx(styles.contact, 'position-absolute', 'd-flex')}>
+                    <div className={styles.phone}><img src={phone} alt=""/></div>
+                    <div className={clsx(styles.contact)}> <h4>9999.999.999</h4></div>
                 </div>
             </div>
-            <div className="about-us ">
+            <div className={clsx(styles.aboutUs)}>
                 <div className="about-us-content pt-5 col-8 m-auto">
                     <h2 className="text-center title">NGHỆ NHÂN TRÀ ĐẠO</h2>
                     <div className="list-person d-flex justify-content-between pt-5">
-                        <div className="person">
+                        <div className={clsx(styles.person)}>
                             <img src={person01} alt=""/>
                             <h4 className="text-center">TS. Trương Huy Hoàng</h4>
                             <p className="text-center">
                             Một chuyên gia sành sỏi về lịch sử ra đời, khoa học và nghệ thuật của trà.</p>
                         </div>
-                        <div className="person">
+                        <div className={clsx(styles.person)}>
                             <img src={person02} alt=""/>
                             <h4 className="text-center">NSND. Lại Gia Huy</h4>
                             <p className="text-center">
                             Sự sáng tạo của ông không chỉ nằm ở việc tạo ra những loại trà mới mẻ mà còn là trong cách kết hợp hương vị độc đáo                            </p>
                         </div>
-                        <div className="person">
+                        <div className={clsx(styles.person)}>
                             <img src={person03} alt=""/>
                             <h4 className="text-center">NSND. Vũ Thị Anh Thư</h4>
                             <p className="text-center">
                             Nghệ nhân có 40 năm kinh nghiệm trồng và chăm sóc những giống trà quý hiếm
                             </p>
                         </div>
-                        <div className="person">
+                        <div className={clsx(styles.person)}>
                             <img src={person04} alt=""/>
                             <h4 className="text-center">TS. Nguyễn Quý Hùng</h4>
                             <p className="text-center">
                             Người đã đặt dấu ấn sâu đậm trong nghệ thuật ẩm thực trà bằng sự tinh tế trong việc phối hợp các loại lá trà và hương liệu              </p>
                         </div>
-                        <div className="person">
+                        <div className={clsx(styles.person)}>
                             <img src={person05} alt=""/>
                             <h4 className="text-center">NSND. Nguyễn Đinh Hoàng Mi </h4>
                             <p className="text-center">
@@ -109,11 +110,11 @@ function Home(){
                     </div>
                 </div>
             </div>
-            <div className="buy pt-5 ">
-                <h2 className="text-center pt-5 title">MUA NGAY</h2>
-                <div className="form d-flex m-auto justify-content-between pt-5">
+            <div className={clsx(styles.buy, 'pt-5')}>
+                <h2 className={clsx(styles.title, 'text-center', 'pt-5')}>MUA NGAY</h2>
+                <div className={clsx(styles.form,  'd-flex', 'm-auto', 'justify-content-between', 'pt-5')}>
                     <input type="text" placeholder="Số điện thoại/Email"/>
-                    <div className="btn">
+                    <div className={clsx(styles.btn)}>
                         GỬI
                     </div>
                 </div>
--- trasenbachdiep-client/src/pages/home/carousel/Carousel.js
+++ trasenbachdiep-client/src/pages/home/carousel/Carousel.js
@@ -3,7 +3,66 @@ import Carousel from 'react-bootstrap/Carousel';
 import banner from '../../../images/trangchu/banner.png'
 import banner01 from '../../../images/Gioithieu/banner.png'
 import { Link } from 'react-router-dom';
+import styled from 'styled-components'
 
+
+const StyledCarousel = styled.div `
+.carousel-indicators{
+  margin-bottom: -3rem;
+}
+.carousel-indicators [data-bs-target] {
+  width: 2rem;
+  height: 2rem;
+  border-radius: 100%;
+  border: 3px solid #aabc44;
+}
+
+.carousel-indicators .active {
+  background-color:#aabc44 ;
+}
+.carousel-control-prev-icon, .carousel-control-next-icon {
+  background-color: #ab4;
+  border-radius: 100%;
+  opacity: .8;
+}
+.carousel-caption{
+  top: 15%;
+  left: 18%;
+  width: 35%;
+}
+.carousel-caption p{
+  width: 70%;
+  font-size: 1.2rem;
+  text-align: justify;
+}
+.carousel-item {
+  height: 546px
+}
+.carousel-item h5{
+  font-size: 2rem;
+  color: #000;
+  font-family: 'SVNAthena';
+}
+.carousel-item h4, .intro-caption h1{
+  font-size: 6rem;
+  color: #43782b;
+  font-family: 'SVNArtfulbeauty';
+  height: 5.5rem;
+}
+.carousel-item .btn, .intro-caption .btn{
+  background-color: #aabc44;
+  width: 11rem;
+  left: 0%;
+}
+.carousel-item .btn a, .intro-caption .btn a{
+  color: #000;
+  font-size: 1.5rem;
+  font-weight: bold;
+  box-sizing: content-box;
+  text-decoration: none;
+}  
+
+`
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
 
@@ -12,6 +71,7 @@ function ControlledCarousel() {
   };
 
   return (
+    <StyledCarousel>
     <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
       <img className="d-block w-100" src={banner} alt="First slide"/>
@@ -47,6 +107,7 @@ function ControlledCarousel() {
         </Carousel.Caption>
       </Carousel.Item>
     </Carousel>
+    </StyledCarousel>
   );
 }
 
